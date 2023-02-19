3421365fc260f5847b960c3b8af3375478d6714e
Chima-Anason <tblynxx@gmail.com>
2023-02-19 12:43:27 +0800

    Initial Commit

---
+++ ChimaCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChimaCard
+//
+//  Created by mac on 19/02/2023.
+//  Copyright © 2023 Chima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
